99ae29a97f8de17df28a6c389f6769c155cc9e7e
uday-majhi <umajhi1@gmail.com>
2024-05-28 13:07:42 +0545

    added the value of a and printed out

--- src/main/java/org/example/practice.java
+++ src/main/java/org/example/practice.java
@@ -3,5 +3,7 @@ package org.example;
 public class practice   {
     public static void main(String[] args) {
         System.out.println("HHH");
+        int a = 5;
+        System.out.println(a);
     }
 }
